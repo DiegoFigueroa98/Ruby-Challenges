8454e70436f046af9566b8cf5287053e538ba7e1
Diego Aaron Figueroa Campos <16460315@colima.tecnm.mx>
2020-06-09 09:26:09 -0500

    Add number functions program

---
+++ variables.rb
@@ -0,0 +1,12 @@
+# Variables
+ten_int = 10
+ten_float = 10.0
+ten_neg = -10
+
+# Number functions
+puts ten_int.to_f
+puts ten_float.to_i
+puts ten_neg.abs
+puts ten_int.even?
+puts ten_int.next
+puts ten_float.next
